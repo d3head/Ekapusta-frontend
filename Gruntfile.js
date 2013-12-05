c4449152ba609dbca7a6888ba9884aa21f58925f
Oleg <hello@oleg.so>
2013-12-06 04:01:46 +0700

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -320,7 +320,7 @@ module.exports = function (grunt) {
     // Run some tasks in parallel to speed up the build process
     concurrent: {
       server: [
-        'compass:server',
+        //'compass:server',
         'copy:styles'
       ],
       test: [
