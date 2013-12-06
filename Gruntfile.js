4f1230ed2dc32a7d6c1ce51e54ba0ea12a87d5ba
Oleg <hello@oleg.so>
2013-12-06 18:29:49 +0700

    1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 // Generated on 2013-07-21 using generator-angular 0.3.0
 'use strict';
-var LIVERELOAD_PORT = 35729;
-var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
 };
